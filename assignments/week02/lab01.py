5c1945543214690ee34c839e9fcfcc2f0b41788a
Neeranut Yimnoi <neeranut.y@ku.th>
2025-07-17 15:11:24 +0700

    week2

--- assignments/week02/lab01.py
+++ assignments/week02/lab01.py
@@ -18,23 +18,18 @@ Below 18.5: Underweight
 
 """
 
+weight = float(input("Weight: "))
+weight = float(input("Height: "))
+bmi = weight / height ** 2
+print("Your BMI: ", bmi)
+if bmi < 18.5:
+    print("Underweight")
 
-"""
-BMI Calculator (20 points)
+if bmi >= 18.5 and bmi <= 24.9:
+    print("Normal Weight")
 
-Write a program that:
+if bmi >= 25.0 and bmi <= 29.9:
+    print("Overweight")
 
-Asks for weight in kilograms
-Asks for height in meters
-Calculates BMI using formula: BMI = weight / (height²)
-Displays BMI with 1 decimal place
-Shows BMI category based on the ranges below
-
-BMI Categories:
-
-Below 18.5: Underweight
-18.5 - 24.9: Normal weight
-25.0 - 29.9: Overweight
-30.0 and above: Obese
-
-"""
+if bmi >= 30.0:
+    print("Obese")
