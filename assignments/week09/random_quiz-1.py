2ce63f8cc902ecbb88ec2ff7dea6f728ce20d801
Neeranut Yimnoi <neeranut.y@ku.th>
2025-09-18 16:00:32 +0700

    week9

--- assignments/week09/random_quiz-1.py
+++ assignments/week09/random_quiz-1.py
@@ -25,3 +25,27 @@ Example
     Congratulations! You won in 3 attempts!
 
 """
+
+import random
+
+# สุ่มเลขระหว่าง 0 - 9
+test_random = random.randint(1, 20)
+
+print("-- เกมทายตัวเลข มาเดาใจคอมพิวเตอร์กันเถอะ --")
+print("-- ทายเลขจำนวนเต็มตั้งแต่ 1 - 20 --")
+print("-- คุณมีโอกาส 6 ครั้ง --")
+
+for i in range(6):
+
+    # รับค่าการทายเลขจากผู้ใช้
+    print(f"ความพยายามครั้งที่ {i+1}")
+    guess_number = int(input("What is your guess num (1-20)?: "))
+
+    # condition ==> if-elif-else
+    if test_random == guess_number:
+       print("ยูเก่งมาก มั่วถูกตั้งแต่ครั้งแรกเลย เทพจริมๆ")
+       break
+    elif guess_number < test_random:
+       print("ผิดจ้า น้อยไปเนอะ")
+    elif guess_number > test_random:
+       print("ผิดจ้า มากไปหน่อย")
--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,19 @@
 import random
 
-def test_random():
-    random_number = random.randint(1, 100)
-    print(random_number)
-    
-test_random()
+# สุ่มเลขระหว่าง 0 - 9
+test_random = random.randint(0, 9)
+
+print("-- เกมทายตัวเลข มาเดาใจคอมพิวเตอร์กันเถอะ --")
+
+# รับค่าการทายเลขจากผู้ใช้
+guess_number = int(input("What is your guess num"))
+
+# condition ==> if-elif-else
+if test_random == guess_number:
+    print("ยูเก่งมาก มั่วถูกตั้งแต่ครั้งแรกเลย เทพจริมๆ")
+
+elif guess_number < test_random:
+    print("ผิดจ้า น้อยไปเนอะ")
+
+elif guess_number > test_random:
+    print("ผิดจ้า มากไปหน่อย")
